a68cdaa974d0c85789cd7520eb24153c0c16e246
Eugene <59817721+SKZGx@users.noreply.github.com>
2023-10-21 13:10:14 +0300

    modified:   about.html 	modified:   addtranslation.html 	new file:   app.py 	renamed:    about.css -> css/about.css 	renamed:    addtranslation.css -> css/addtranslation.css 	renamed:    load.css -> css/load.css 	renamed:    notcompleted.css -> css/notcompleted.css 	renamed:    scrollbar.css -> css/scrollbar.css 	renamed:    search.css -> css/search.css 	new file:   css/styles.css 	renamed:    submited.css -> css/submited.css 	modified:   index.html 	renamed:    about.js -> js/about.js 	renamed:    index.js -> js/index.js 	renamed:    script.js -> js/script.js 	renamed:    search.js -> js/search.js 	renamed:    tabs.js -> js/tabs.js 	renamed:    tabsindex.js -> js/tabsindex.js 	modified:   mods.json 	new file:   static/addtranslation.css 	new file:   static/default-image.png 	new file:   static/icon.png 	new file:   static/script.js 	new file:   static/style.css 	renamed:    styles.css -> static/styles.css 	modified:   submited.html 	new file:   templates/editor.html

---
+++ app.py
@@ -0,0 +1,149 @@
+from flask import Flask, render_template, request, jsonify
+import json
+import urllib.parse
+from jinja2 import Environment
+from colorama import Fore, Style  # Додайте імпорт кольорових стилів
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+
+app = Flask(__name__)
+app.debug = True
+
+# Зчитування даних з JSON файлу
+data_file = "mods.json"  # Шлях до вашого JSON-файлу
+
+def read_data_from_file():
+    try:
+        with open(data_file, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        return []
+
+def save_data_to_file(data):
+    with open(data_file, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+# Додавання фільтра urlparse до Jinja2
+def urlparse(url):
+    from urllib.parse import urlparse
+    netloc = urlparse(url).netloc
+    if netloc.startswith("www."):
+        netloc = netloc[4:]  # Видаляємо "www." з початку
+    if netloc.endswith(".com"):
+        netloc = netloc[:-4]  # Видаляємо ".com" з кінця
+    return netloc
+
+app.jinja_env.filters['urlparse'] = urlparse
+
+@app.route('/')
+def editor():
+    data = read_data_from_file()
+    return render_template('editor.html', data=data)
+
+@app.route('/delete_card', methods=['POST'])
+def delete_card_handler():
+    data = request.get_json()  # Отримання даних з POST-запиту у форматі JSON
+    card_title = data.get('title')
+
+    if card_title is not None:
+        print(f'{Fore.YELLOW}Стараємось видалити картку: {card_title}{Style.RESET_ALL}')
+        data = read_data_from_file()
+        for card in data:
+            if 'title' in card and card['title'] == card_title:
+                data.remove(card)
+                save_data_to_file(data)
+                print(f'{Fore.GREEN}Видалено картку: {card_title}{Style.RESET_ALL}')  # Повідомлення про видалену картку у консоль
+                return '', 204  # Успішна відповідь без тіла
+
+    return f'{Fore.RED}Картку не знайдено{Style.RESET_ALL}', 404
+
+@app.route('/add_card', methods=['POST'])
+def add_card():
+    data = request.get_json()  # Отримання даних з POST-запиту у форматі JSON
+    new_title = data.get('title')
+    new_description = data.get('description')
+    new_image = data.get('image')
+    new_author = data.get('author')
+    new_verified = data.get('verified')
+    new_completed = data.get('completed')
+    new_link = data.get('link')
+    
+    print(f'{Fore.GREEN}Додано нову картку: {new_title} з даними {data}{Style.RESET_ALL}')  # Повідомлення про додану картку у консоль
+
+    if not new_title:
+        return f'{Fore.RED}Помилка: Запис не має заголовку.{Style.RESET_ALL}', 400
+
+    data = read_data_from_file()
+
+    # Перевірка, чи не існує картки з таким же title
+    for card in data:
+        if card['title'] == new_title:
+            return f'{Fore.RED}Картка з таким заголовком вже існує{Style.RESET_ALL}', 400
+
+    # Генерування нового унікального 'id'
+    new_id = len(data) + 1
+
+    new_card = {
+        'title': new_title,
+        'description': new_description,
+        'image': new_image,
+        'verified': new_verified,
+        'author': new_author,
+        'completed': new_completed,
+        'link': new_link
+    }
+
+    data.append(new_card)
+    save_data_to_file(data)
+    return jsonify(new_card)
+
+@app.route('/edit_card', methods=['POST'])
+def edit_card():
+    if request.content_type != 'application/json':
+        return f'{Fore.RED}Непідтримуваний тип медіа{Style.RESET_ALL}', 415
+
+    data = request.get_json()
+    card_title = data.get('title')
+
+    if card_title is not None:
+        print(f'{Fore.YELLOW}Стараємось оновити: {card_title}{Style.RESET_ALL}')
+        new_title = urllib.parse.unquote(data.get('newTitle', ''))
+        new_description = urllib.parse.unquote(data.get('newDescription', ''))
+        new_image = urllib.parse.unquote(data.get('newImage', ''))
+        new_author = urllib.parse.unquote(data.get('newAuthor', ''))
+        new_verified = data.get('newVerified', False)
+        new_completed = data.get('newCompleted', False)
+        new_link = urllib.parse.unquote(data.get('newLink', ''))
+
+        data = read_data_from_file()
+
+        updated = False
+        updated_card = None
+
+        for card in data:
+            if 'title' in card and card['title'] == card_title:
+                card['title'] = new_title
+                card['description'] = new_description
+                card['image'] = new_image
+                card['author'] = new_author
+                card['verified'] = new_verified
+                card['completed'] = new_completed
+                card['link'] = new_link
+                updated = True
+                updated_card = card
+                break
+
+        if updated:
+            save_data_to_file(data)
+            print(f'{Fore.GREEN}Оновлено картку: {new_title}{Style.RESET_ALL}')
+            return jsonify(updated_card)  # Повернення зміненої картки у відповіді
+        else:
+            return f'{Fore.RED}Картку не знайдено{Style.RESET_ALL}', 404
+    else:
+        return f'{Fore.RED}Некоректні дані{Style.RESET_ALL}', 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,263 @@
+$(document).ready(function () {
+    $('.edit-button').click(function () {
+        const cardDiv = $(this).closest('.card');
+        const form = cardDiv.find('.form');
+        form.toggle();
+        const cardTitle = cardDiv.data('title');
+        form.find('.new-title').val(cardTitle);
+    });
+
+    $('.site-link').each(function () {
+        const fullLink = $(this).attr('href');
+        if (fullLink) {
+            const hostname = new URL(fullLink).hostname;
+            $(this).text(hostname);
+        }
+    });
+
+    function deleteCard(cardDiv) {
+        const cardTitle = cardDiv.data('title');
+        if (confirm(`Видалити запис "${cardTitle}"? Дані запису видаляться назавжди.`)) {
+            $.ajax({
+                type: 'POST',
+                url: '/delete_card',
+                data: JSON.stringify({ title: cardTitle }),
+                contentType: 'application/json',
+                success: function () {
+                    cardDiv.remove();
+                },
+            });
+        }
+    }
+
+    function saveUpdatedCard(cardDiv, form) {
+        const cardTitle = cardDiv.data('title');
+        const cardTitleElement = cardDiv.find('h3');
+        const cardDescriptionElement = cardDiv.find('p');
+        const cardImageElement = cardDiv.find('.imageitem');
+        const cardAuthorElement = cardDiv.find('.author');
+        const cardVerifiedElement = cardDiv.find('.ver');
+        const cardCompletedElement = cardDiv.find('.ove');
+        const cardLinkElement = cardDiv.find('.site-linka');
+    
+        let newTitle = form.find('.new-title').val();
+        let newDescription = form.find('.new-description').val();
+        let newImage = form.find('.new-image').val();
+        let newAuthor = form.find('.new-author').val();
+        let newVerified = form.find('.new-verified').prop('checked');
+        let newCompleted = form.find('.new-completed').prop('checked');
+        let newLink = form.find('.new-link').val();
+    
+        const currentLink = cardLinkElement.attr('href') || '';
+    
+        const isDataChanged =
+            newTitle !== cardTitleElement.text() ||
+            newDescription !== cardDescriptionElement.text() ||
+            newImage !== cardImageElement.attr('src') ||
+            newAuthor !== cardAuthorElement.text() ||
+            newVerified !== cardVerifiedElement.text().includes('Так') ||
+            newCompleted !== cardCompletedElement.text().includes('Так') ||
+            newLink !== currentLink;
+    
+        if (!isDataChanged) {
+            form.hide();
+            return;
+        }
+    
+        const formData = {
+            title: cardTitle,
+        };
+    
+        if (newTitle) {
+            formData.newTitle = encodeURIComponent(newTitle);
+        }
+        if (newDescription) {
+            formData.newDescription = encodeURIComponent(newDescription);
+        }
+        if (newImage) {
+            formData.newImage = encodeURIComponent(newImage);
+        }
+        if (newAuthor) {
+            formData.newAuthor = encodeURIComponent(newAuthor);
+        }
+        if (newVerified) {
+            formData.newVerified = newVerified;
+        }
+        if (newCompleted) {
+            formData.newCompleted = newCompleted;
+        }
+        if (newLink && newLink !== '#') {
+            formData.newLink = encodeURIComponent(newLink);
+        }
+    
+        $.ajax({
+            type: 'POST',
+            url: '/edit_card',
+            data: JSON.stringify(formData),
+            contentType: 'application/json',
+            success: function (response) {
+                form.hide();
+                if (formData.newTitle) {
+                    cardTitleElement.text(decodeURIComponent(response.title));
+                }
+                if (formData.newDescription) {
+                    cardDescriptionElement.text(decodeURIComponent(response.description));
+                }
+                if (formData.newImage) {
+                    cardImageElement.attr('src', decodeURIComponent(response.image));
+                }
+                if (formData.newAuthor) {
+                    cardAuthorElement.text(decodeURIComponent(response.author));
+                }
+                if (formData.newVerified) {
+                    cardVerifiedElement.text('Офіційно: ' + (response.verified ? 'Так' : 'Ні'));
+                }
+                if (formData.newCompleted) {
+                    cardCompletedElement.text('Завершено: ' + (response.completed ? 'Так' : 'Ні'));
+                }
+                if (formData.newLink) {
+                    cardLinkElement.attr('href', decodeURIComponent(response.link));
+                }
+            },
+        });
+    }
+    
+
+    $('.save-button').click(function () {
+        const cardDiv = $(this).closest('.card');
+        const form = cardDiv.find('.form');
+        saveUpdatedCard(cardDiv, form);
+    });
+
+    $('.cancel-button').click(function () {
+        const form = $(this).closest('.form');
+        form.hide();
+    });
+
+    $('.add-button').click(function () {
+        const newTitle = $('#new-title').val();
+        const newDescription = $('#new-description').val();
+        const newImage = $('#new-image').val();
+        const newAuthor = $('#new-author').val();
+        const newLink = $('#new-link').val();
+        const newVerified = $('#new-verified').prop('checked');
+        const newCompleted = $('#new-completed').prop('checked');
+
+        const titleRegex = /^[^0-9!@#$%^&*()][A-Za-zА-Яа-яЁёҐґЄєЇїІі0-9\s]*$/;
+
+        if (!titleRegex.test(newTitle)) {
+            alert('Помилка: Заголовок повинен починатися не з цифри чи спеціального символу.');
+            return;
+        }
+
+        const formData = JSON.stringify({
+            title: newTitle,
+            description: newDescription,
+            image: newImage,
+            verified: newVerified,
+            author: newAuthor,
+            completed: newCompleted,
+            link: newLink,
+        });
+
+        $.ajax({
+            type: 'POST',
+            url: '/add_card',
+            data: formData,
+            contentType: 'application/json',
+            success: function (response) {
+                const newCardHtml = `
+                    <!-- Ваш HTML-код для нової карточки -->
+                `;
+                const newCard = $(newCardHtml);
+                $('.card-container').append(newCard);
+
+                newCard.find('.delete-button').click(function () {
+                    deleteCard(newCard);
+                });
+                newCard.find('.edit-button').click(function () {
+                    editCard(newCard);
+                });
+
+                $('#new-title').val('');
+                $('#new-description').val('');
+                $('#new-image').val('');
+                $('#new-author').val('');
+                $('#new-link').val('');
+                $('#new-verified').prop('checked', false);
+                $('#new-completed').prop('checked', false);
+            },
+        });
+    });
+
+    function editCard(card) {
+        const form = card.find('.form');
+        form.show();
+        const cardTitle = card.data('title');
+        const currentTitle = card.find('h3').text();
+        const currentDescription = card.find('p').text();
+        const currentImage = card.find('.imageitem').attr('src');
+        const currentAuthor = card.find('.author').text();
+        const currentVerified = card.find('.ver').text().includes('Так');
+        const currentCompleted = card.find('.ove').text().includes('Так');
+        const currentLink = card.find('.link').find('a').attr('href');
+
+        form.find('.new-title').val(currentTitle);
+        form.find('.new-description').val(currentDescription);
+        form.find('.new-image').val(currentImage);
+        form.find('.new-author').val(currentAuthor);
+        form.find('.new-verified').prop('checked', currentVerified);
+        form.find('.new-completed').prop('checked', currentCompleted);
+        form.find('.new-link').val(currentLink);
+    }
+
+    $('.edit-button').click(function () {
+        editCard($(this).closest('.card'));
+    });
+
+    
+
+    function sortCardsAZ() {
+        const cardContainer = $('.card-container');
+        const cards = cardContainer.find('.card').get();
+        cards.sort(function (a, b) {
+            const titleA = $(a).data('title').toLowerCase();
+            const titleB = $(b).data('title').toLowerCase();
+            return titleA.localeCompare(titleB);
+        });
+        cardContainer.empty();
+        $.each(cards, function (i, card) {
+            cardContainer.append(card);
+        });
+    }
+
+    function sortCardsZA() {
+        const cardContainer = $('.card-container');
+        const cards = cardContainer.find('.card').get();
+        cards.sort(function (a, b) {
+            const titleA = $(a).data('title').toLowerCase();
+            const titleB = $(b).data('title').toLowerCase();
+            return titleB.localeCompare(titleA);
+        });
+        cardContainer.empty();
+        $.each(cards, function (i, card) {
+            cardContainer.append(card);
+        });
+    }
+
+    $('#sort-az-button').click(sortCardsAZ);
+    $('#sort-za-button').click(sortCardsZA);
+
+    function setupCardButtons() {
+        // Add event handlers for any dynamically added card buttons
+        $('.card-container .delete-button').click(function () {
+            deleteCard($(this).closest('.card'));
+        });
+
+        $('.card-container .edit-button').click(function () {
+            editCard($(this).closest('.card'));
+        });
+    }
+
+    setupCardButtons();
+});
