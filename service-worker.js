0c1363c155b0cac6052b071a56983072308e2687
Eugene <59817721+SKZGx@users.noreply.github.com>
2023-12-06 19:07:30 +0200

    modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,69 @@
 const cacheName = 'SUMTRANSLATE_REL_1';
 const maxImageAge = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
-
-// Domains to retrieve and cache images from
-const imageDomainsToCache = [
-  'curseforge.com',
-  'modrinth.com',
-  'imgur.com',
-  'i.ibb.co',
-  'github.com'
+// Files to cache
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://i.ibb.co/tQsvMtK/android.png',
+  // Add more paths to your static assets here
 ];
 
-// Function to retrieve and cache images
-function cacheImages() {
-  imageDomainsToCache.forEach(domain => {
-    // Example: create an image element to trigger browser image caching
-    const img = new Image();
-    
-    // Example URL, replace with the actual image URL
-    const imageUrl = `https://${domain}/path/to/image.png`;
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
 
-    // Set the image source to trigger the fetch and caching
-    img.src = imageUrl;
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((existingCacheName) => {
+          if (existingCacheName !== cacheName) {
+            return caches.delete(existingCacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
-    // Listen for the 'load' event to confirm image loading
-    img.addEventListener('load', () => {
-      // Image has been loaded and cached
-      console.log(`Image cached: ${imageUrl}`);
-    });
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-    // Listen for the 'error' event to handle fetch errors
-    img.addEventListener('error', (error) => {
-      console.error(`Failed to fetch resource: ${imageUrl}`, error);
-    });
-  });
-}
+        // Clone the request because it's a stream and can only be consumed once
+        const fetchRequest = event.request.clone();
 
-// Call the function to cache images
-cacheImages();
+        return fetch(fetchRequest).then((response) => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response to cache it
+          const responseToCache = response.clone();
+
+          caches.open(cacheName)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+    );
+});
