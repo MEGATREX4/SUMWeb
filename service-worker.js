e7128d707da5a7991ea752ff86ee64877151110b
Eugene <59817721+SKZGx@users.noreply.github.com>
2023-12-06 19:34:47 +0200

    modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'SUMTRANSLATE_REL_2'; // Update cache name to trigger the installation of a new service worker
+const cacheName = 'SUMTRANSLATE_REL_3'; // Update cache name to trigger the installation of a new service worker
 const maxImageAge = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
 // Included domains for image caching
@@ -19,6 +19,8 @@ const filesToCache = [
 
 // Install event
 self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+
   event.waitUntil(
     caches.open(cacheName)
       .then((cache) => {
@@ -29,6 +31,8 @@ self.addEventListener('install', (event) => {
 
 // Activate event
 self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -44,6 +48,8 @@ self.addEventListener('activate', (event) => {
 
 // Fetch event
 self.addEventListener('fetch', (event) => {
+  console.log('Fetch event intercepted:', event.request.url);
+
   const requestUrl = new URL(event.request.url);
   const isIncludedImageDomain = includedImageDomains.some(domain => requestUrl.hostname.includes(domain));
 
@@ -52,6 +58,7 @@ self.addEventListener('fetch', (event) => {
       .then((response) => {
         // Cache hit - return response
         if (response) {
+          console.log('Cache hit:', event.request.url);
           return response;
         }
 
