561320c11c7383516eca139faa3b612bbcec3105
Eugene <59817721+SKZGx@users.noreply.github.com>
2023-12-06 19:12:18 +0200

    modified:   service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,121 +1,69 @@
 const cacheName = 'SUMTRANSLATE_REL_1';
 const maxImageAge = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
-// Domains to automatically cache images from
-const imageDomainsToCache = [
-  'curseforge.com',
-  'modrinth.com',
-  'imgur.com',
-  'i.ibb.co',
-  'github.com'
+// Files to cache
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://i.ibb.co/tQsvMtK/android.png',
+  // Add more paths to your static assets here
 ];
 
 // Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      // Cache only image files from specific domains
-      return cache.addAll([]);
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((existingCacheName) => {
+          if (existingCacheName !== cacheName) {
+            return caches.delete(existingCacheName);
+          }
+        })
+      );
     })
   );
 });
 
 // Fetch event
 self.addEventListener('fetch', (event) => {
-  const requestUrl = new URL(event.request.url);
-  const isImageFromAllowedDomain = imageDomainsToCache.some(domain => requestUrl.hostname.includes(domain));
-
-  if (isImageFromAllowedDomain) {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
         if (response) {
-          // Check if the cached image is too old
-          const cachedDate = new Date(response.headers.get('date'));
-          const currentDate = new Date();
-          if (currentDate - cachedDate > maxImageAge) {
-            // Image is older than the threshold, refresh it
-            return fetchAndCache(event.request);
-          }
           return response;
-        } else {
-          // Image is not in the cache, fetch it and cache it
-          return fetchAndCache(event.request);
         }
+
+        // Clone the request because it's a stream and can only be consumed once
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then((response) => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response to cache it
+          const responseToCache = response.clone();
+
+          caches.open(cacheName)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
       })
     );
-  }
-});
-
-function fetchAndCache(request) {
-  return fetch(request)
-    .then((response) => {
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-        console.error(`Failed to fetch resource: ${request.url}`, response);
-        throw new Error('Failed to fetch resource');
-      }
-
-      // Clone the response to cache it
-      const responseToCache = response.clone();
-
-      caches.open(cacheName).then((cache) => {
-        cache.put(request, responseToCache);
-      });
-
-      return response;
-    })
-    .catch((error) => {
-      console.error(`Fetch error for resource: ${request.url}`, error);
-      throw error;
-    });
-}
-
-
-
-function displayCacheSize() {
-  caches.open(cacheName).then((cache) => {
-    cache.keys().then((keys) => {
-      const size = keys.reduce((acc, key) => {
-        return acc + key.url.length;
-      }, 0);
-      console.log(`Cache size: ${Math.round(size / 1024)} KB`);
-    });
-  });
-}
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll([]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request).then((response) => {
-        if (
-          !response ||
-          response.status !== 200 ||
-          response.type !== 'basic' ||
-          !imageDomainsToCache.some((domain) => event.request.url.includes(domain))
-        ) {
-          return response;
-        }
-
-        const responseToCache = response.clone();
-
-        caches.open(cacheName).then((cache) => {
-          cache.put(event.request, responseToCache);
-          displayCacheSize(); // Call the function to display cache size
-        });
-
-        return response;
-      });
-    })
-  );
 });
