3a9b5a5e66a1d50bc2a64ad12e4f9ef58b4d3441
Eugene <59817721+SKZGx@users.noreply.github.com>
2023-12-06 18:19:50 +0200

    modified:   about.html 	modified:   addtranslation.html 	modified:   index.html 	modified:   service-worker.js 	deleted:    sitemap2.xml 	deleted:    sitemap_4403020.xml 	modified:   socials.html 	modified:   submited.html

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,33 @@
-const cacheName = 'SUMTRANSLATE BETA 0.9';
+const cacheName = 'SUMTRANSLATE_BETA_0.9';
 const maxImageAge = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
-// Files to cache
-const imageFilesToCache = [
-  'https://i.ibb.co/tQsvMtK/android.png',
-  // Add more image URLs to be cached here
+// Domains to automatically cache images from
+const imageDomainsToCache = [
+  'curseforge.com',
+  'modrinth.com',
+  'imgur.com',
+  'i.ibb.co',
+  'github.com'
 ];
 
 // Install event
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        // Cache only image files
-        return cache.addAll(imageFilesToCache);
-      })
-  );
-});
-
-// Activate event
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((existingCacheName) => {
-          if (existingCacheName !== cacheName) {
-            return caches.delete(existingCacheName);
-          }
-        })
-      );
+    caches.open(cacheName).then((cache) => {
+      // Cache only image files from specific domains
+      return cache.addAll([]);
     })
   );
 });
 
 // Fetch event
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
+  const requestUrl = new URL(event.request.url);
+  const isImageFromAllowedDomain = imageDomainsToCache.some(domain => requestUrl.hostname.includes(domain));
+
+  if (isImageFromAllowedDomain) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
         if (response) {
           // Check if the cached image is too old
           const cachedDate = new Date(response.headers.get('date'));
@@ -47,34 +37,27 @@ self.addEventListener('fetch', (event) => {
             return fetchAndCache(event.request);
           }
           return response;
-        }
-
-        const url = new URL(event.request.url);
-
-        if (imageFilesToCache.includes(url.href)) {
+        } else {
           // Image is not in the cache, fetch it and cache it
           return fetchAndCache(event.request);
-        } else {
-          // For non-image files, fetch them directly from the network
-          return fetch(event.request);
         }
       })
     );
+  }
 });
 
 function fetchAndCache(request) {
   return fetch(request).then((response) => {
     if (!response || response.status !== 200 || response.type !== 'basic') {
-      return response;
+      throw new Error('Failed to fetch resource');
     }
 
     // Clone the response to cache it
     const responseToCache = response.clone();
 
-    caches.open(cacheName)
-      .then((cache) => {
-        cache.put(request, responseToCache);
-      });
+    caches.open(cacheName).then((cache) => {
+      cache.put(request, responseToCache);
+    });
 
     return response;
   });
