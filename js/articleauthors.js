3c144827294dc0e01a45532a4fac26fe426c9017
Eugene <59817721+SKZGx@users.noreply.github.com>
2024-04-29 12:22:26 +0300

    articleauthors speed up 	modified:   FAQ.html 	modified:   about.html 	modified:   js/articleauthors.js

--- js/articleauthors.js
+++ js/articleauthors.js
@@ -1,34 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get all the divs for displaying the author
-    var authorDivs = document.querySelectorAll(".articleauthor");
+$(document).ready(function() {
+    // Select all divs for displaying the author
+    var authorDivs = $(".articleauthor");
 
     // Iterate over each author div
-    authorDivs.forEach(function(authorDiv) {
-        // Check if the author div exists and has an id
-        if (authorDiv && authorDiv.id) {
-            // Load data from the JSON file using Fetch API
-            fetch('articleauthors.json')
-                .then(response => response.json())
-                .then(data => {
-                    // Find the author data corresponding to the author id
-                    var authorData = data.find(author => author.authorID === authorDiv.id);
-                    
-                    // If author data is found, construct the author HTML
-                    if (authorData) {
-                        var authorHTML = `
-                            <div class="author">
-                                <img src="${authorData.authorImage}" alt="${authorData.authorName}" class="author-image">
-                                <div class="author-info">
-                                    <p class="author-name">${authorData.authorName}</p>
-                                    <p style="font-size: 12px;" class="author-about">${authorData.authorAbout}</p>
-                                </div>
-                            </div>`;
+    authorDivs.each(function() {
+        var authorDiv = $(this);
+        var authorID = authorDiv.attr("id");
 
-                        // Set the author div's innerHTML to display the author information
-                        authorDiv.innerHTML = authorHTML;
-                    }
-                })
-                .catch(error => console.error('Error loading author data:', error));
-        }
+        // Load data from the JSON file using Axios
+        axios.get('articleauthors.json')
+            .then(function(response) {
+                var data = response.data;
+
+                // Find the author data corresponding to the author id
+                var authorData = data.find(function(author) {
+                    return author.authorID === authorID;
+                });
+
+                // If author data is found, construct the author HTML
+                if (authorData) {
+                    var authorHTML = `
+                        <div class="author">
+                            <img src="${authorData.authorImage}" alt="${authorData.authorName}" class="author-image">
+                            <div class="author-info">
+                                <p class="author-name">${authorData.authorName}</p>
+                                <p style="font-size: 12px;" class="author-about">${authorData.authorAbout}</p>
+                            </div>
+                        </div>`;
+
+                    // Set the author div's HTML to display the author information
+                    authorDiv.html(authorHTML);
+                }
+            })
+            .catch(function(error) {
+                console.error('Error loading author data:', error);
+            });
     });
 });
